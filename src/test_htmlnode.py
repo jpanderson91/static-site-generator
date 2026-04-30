fddc9101a0bf0f8608f3d3c9287297d060dee056
John_Anderson <John.anderson@lsgskychefs.com>
2026-04-30 09:16:01 -0500

    added LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -29,6 +29,29 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("p", "hello", None, {"class": "text"})
         self.assertEqual(repr(node), "HTMLNode(p, hello, None, {'class': 'text'})")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just raw text")
+        self.assertEqual(node.to_html(), "Just raw text")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_repr(self):
+        node = LeafNode("a", "link", {"href": "https://example.com"})
+        self.assertEqual(repr(node), "LeafNode(a, link, {'href': 'https://example.com'})")
 
 
 if __name__ == "__main__":
