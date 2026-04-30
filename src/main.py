cf5e5bcd4e736939a0fd0990d7a8ba671e1f0f4f
John_Anderson <John.anderson@lsgskychefs.com>
2026-04-30 07:59:39 -0500

    added textnode and test_textnode

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# hello world
+# create a main() function in main.py and call it. The function should create a new TextNode object with some dummy values. 
+# Print the object, and make sure it looks like you'd expect. For example:
+# TextNode(This is some anchor text, link, https://www.boot.dev)
+def main():
+    from textnode import TextNode
+    from textnode import Bender
+    
+    node = TextNode("This is some anchor text", Bender.WATER_BENDER, "https://www.boot.dev")
+    print(node)
+
+main()
